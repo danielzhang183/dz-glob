5dc891a93157519f94d0b7dfe8acd6113fae138f
danielzhang183 <danielzhang182@gmail.com>
2023-05-07 09:33:29 +0800

    feat: literal check

--- src/transform.ts
+++ src/transform.ts
@@ -2,52 +2,9 @@ import { dirname } from 'path'
 import fg from 'fast-glob'
 import MagicString from 'magic-string'
 import type { TransformPluginContext } from 'rollup'
-import type { ArrayExpression, Literal, ObjectExpression } from 'estree'
-import type { GlobalOptions, ParsedImportGlob } from '../types'
+import { parseImportGlob } from './parse'
 
-const importGlobRE = /\bimport\.meta\.globNext(?:<\w+>)?\(([\s\S]*?)\)/g
 const importPrefix = '__glob_next__'
-
-export function parseImportGlob(
-  code: string,
-  parse: TransformPluginContext['parse'],
-): ParsedImportGlob[] {
-  const matches = Array.from(code.matchAll(importGlobRE))
-
-  return matches.map((match, index) => {
-    const argumentString = `[${match[1]}]`
-    // @ts-expect-error ignore for now
-    const ast = parse(argumentString, { ecmaVersion: 'latest' }).body[0].expression as Literal | ArrayExpression
-    const arg1 = ast.elements[0] as Literal | ArrayExpression
-    const globs: string[] = []
-    if (arg1.type === 'ArrayExpression') {
-      for (const element of arg1.elements) {
-        if (element?.type === 'Literal')
-          globs.push(element!.value as string)
-      }
-    }
-    else {
-      globs.push(arg1.value as string)
-    }
-
-    // arg2
-    const options: GlobalOptions<boolean> = {}
-    const args2 = ast.elements[1] as ObjectExpression | undefined
-    if (args2) {
-      for (const property of args2.properties) {
-        // @ts-expect-error ignore for now
-        options[property.key.name] = property.value.value
-      }
-    }
-
-    return {
-      match,
-      index,
-      globs,
-      options,
-    }
-  })
-}
 
 export async function transform(
   code: string,
---
+++ test/parse.test.ts
@@ -0,0 +1,120 @@
+import { describe, expect, it } from 'vitest'
+import { parse } from 'acorn'
+import { parseImportGlob } from '../src/parse'
+
+function run(input: string) {
+  return parseImportGlob(input, parse).map(i => ({
+    globs: i.globs,
+    options: i.options,
+  }))
+}
+
+function runError(input: string) {
+  try {
+    run(input)
+  }
+  catch (e) {
+    return e
+  }
+
+  throw new Error('Should throw')
+}
+
+describe('parse positive', () => {
+  it('basic', () => {
+    expect(run(`
+    import.meta.globNext(\'./modules/*.ts\')
+    `)).toMatchInlineSnapshot(`
+      [
+        {
+          "globs": [
+            "./modules/*.ts",
+          ],
+          "options": {},
+        },
+      ]
+    `)
+  })
+
+  it('array', () => {
+    expect(run(`
+    import.meta.globNext([\'./modules/*.ts\', './dir/*/{js,ts}\'])
+    `)).toMatchInlineSnapshot(`
+      [
+        {
+          "globs": [
+            "./modules/*.ts",
+            "./dir/*/{js,ts}",
+          ],
+          "options": {},
+        },
+      ]
+    `)
+  })
+
+  it('options with multilines', () => {
+    expect(run(`
+    import.meta.globNext([
+      \'./modules/*.ts\',
+      \'./dir/*.{js,ts}\',
+    ], {
+      eager: true,
+      export: 'named',
+    })
+    `)).toMatchInlineSnapshot(`
+      [
+        {
+          "globs": [
+            "./modules/*.ts",
+            "./dir/*.{js,ts}",
+          ],
+          "options": {
+            "eager": true,
+            "export": "named",
+          },
+        },
+      ]
+    `)
+  })
+})
+
+describe('parse negative', () => {
+  it('variable', () => {
+    expect(runError('import.meta.globNext(hi)'))
+      .toMatchInlineSnapshot('[Error: Could only use literals in import.meta.importGlob]')
+  })
+
+  it('template', () => {
+    // eslint-disable-next-line no-template-curly-in-string
+    expect(runError('import.meta.globNext(`hi ${hey}`)'))
+      .toMatchInlineSnapshot('[Error: Could only use literals in import.meta.importGlob]')
+  })
+
+  it('to be string', () => {
+    expect(runError('import.meta.globNext(1)'))
+      .toMatchInlineSnapshot('[Error: Expected glob to be a string, but got "number"]')
+  })
+
+  it('be array variable', () => {
+    expect(runError('import.meta.globNext([hey])'))
+      .toMatchInlineSnapshot('[Error: Could only use literals in import.meta.importGlob]')
+    expect(runError('import.meta.globNext(["1", hey])'))
+      .toMatchInlineSnapshot('[Error: Could only use literals in import.meta.importGlob]')
+  })
+
+  it('options', () => {
+    expect(runError('import.meta.globNext("hey", hey)'))
+      .toMatchInlineSnapshot('[Error: Expected the second argument of import.meta.importGlob to be a object literal, but got "Identifier"]')
+    expect(runError('import.meta.globNext("hey", [])'))
+      .toMatchInlineSnapshot('[Error: Expected the second argument of import.meta.importGlob to be a object literal, but got "ArrayExpression"]')
+  })
+
+  it('options props', () => {
+    expect(runError('import.meta.globNext("hey", { hey: 1 })'))
+      .toMatchInlineSnapshot('[Error: Unknown options hey]')
+    expect(runError('import.meta.globNext("hey", { expect: hey })'))
+      .toMatchInlineSnapshot('[Error: Could only use literals in import.meta.importGlob]')
+    expect(runError('import.meta.globNext("hey", { eager: 123 })'))
+      .toMatchInlineSnapshot('[Error: Expect the type of option "eager" to be "boolean", but got "number"]')
+  })
+})
